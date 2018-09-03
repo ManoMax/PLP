848ee1eee8385077e53d894cd7421bb101af2dce
ManoMax <gabrielmaxcontato@gmail.com>
2018-09-02 23:18:03 -0300

    First Commit

---
+++ Dirlididi/primeiroProgramaC.cpp
@@ -0,0 +1,39 @@
+//incluíndo biblioteca stdio.h
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+   
+   //Lendo um inteiro em C
+	int i;
+	scanf("%d", &i);
+
+	//Lendo um inteiro em C++
+	int i;
+	cin >> i;
+	
+	//Imprimindo o valor da variável inteira x em C
+	printf("%d", x);
+
+	//Imprimindo o valor da variável inteira x em C++
+	cout << x;
+
+	int Vetor[5];  // declara um vetor de 5 posições
+	int Matriz[5][3]; // declara uma matriz de 5 linhas e 3 colunas
+
+	Vetor[0] = 9; // coloca 9 na primeira posição do vetor
+	Vetor[4] = 30 // coloca 30 na última posição do vetor
+
+	Matriz[0][1] = 15; // coloca 15 na célula que está na primeira linha e na segunda coluna da matriz
+
+	
+
+	return 0;
+}
+
+// Definição da função soma
+void  soma(float a, int b) {
+	float result;     
+	result = a+b;
+	printf("A soma de %f com %d é %6.3f\n, a, b, result); 
+}
